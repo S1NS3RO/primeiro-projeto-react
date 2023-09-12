45ceabbbc24d8fb590466d7d87fba8d1f2e9549e
S1NS3RO <douglassgbr@gmail.com>
2023-09-12 17:41:28 -0300

    format

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import "./styles.css";
 function App() {
   return (
     <main>
-    <section id="um">
-      <h1> helo world!</h1>
-      <BsFillAirplaneEnginesFill size={55} color="#000" />
+      <section id="um">
+        <h1> helo world!</h1>
+        <BsFillAirplaneEnginesFill size={35} color="#000" />
       </section>
       <section id="dois">
-      <h2>section2</h2>
+        <h2>section2</h2>
       </section>
     </main>
   );
